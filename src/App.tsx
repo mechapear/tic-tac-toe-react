757a72dc18fa1110e0ea6e397d4c15337f1dbd30
mechapear <mechapear@gmail.com>
2023-10-09 15:45:20 +0700

    feat: add buttons for going back to a specific state

--- src/App.tsx
+++ src/App.tsx
@@ -15,36 +15,64 @@ export default function Game() {
   // and sets each of them to null
   // Each entry in the array is X or O or null
   const [history, setHistory] = useState(INITIAL_HISTORY)
-  // Render the squares by read the last squares array from the history
-  const currentSquares = history[history.length - 1]
+  const [currentMove, setCurrentMove] = useState(0)
+  // Render the squares
+  const currentSquares = history[currentMove]
 
   // Convert to variable holding arrow function format
   // Add type to variable instead of adding type to parameters and the return value
   // to make sure that the type of parameters and the return value are the same as onPlay type
   const handlePlay: BoardProps['onPlay'] = (nextSquares) => {
+    // Update new move into history after user go back in time
+    // update history by appending the updated squares array as a new history entry
+    const newHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(newHistory)
+    // Update currentMove
+    setCurrentMove(newHistory.length - 1)
     // flip the value of xIsNext for determine the next player
     setXIsNext(!xIsNext)
-    // update history by appending the updated squares array as a new history entry
-    setHistory([...history, nextSquares])
   }
 
   const handleRestartClick: BoardProps['onRestart'] = () => {
-    setHistory(Array(9).fill(null))
+    setHistory(INITIAL_HISTORY)
     setXIsNext(true)
+    setCurrentMove(0)
+  }
+
+  // use map to transform history of moves into React elements representing buttons
+  // _ in front of variable name to indicate that we don't use it
+  const move = history.map((_squares, move: number) => {
+    let description
+    if (move > 0) {
+      description = 'Go to move #' + move
+      return (
+        // Useing move as a key
+        // Moves will never be re-ordered, deleted, or inserted, so it’s safe to use the move index as a key
+        <li key={move} className="jump-button-item">
+          <button className="jump-button" onClick={() => jumpTo(move)}>
+            {description}
+          </button>
+        </li>
+      )
+    }
+  })
+
+  function jumpTo(nextMove: number) {
+    setCurrentMove(nextMove)
+    // set xIsNext to true if the number that you’re changing currentMove to is even.
+    setXIsNext(nextMove % 2 === 0)
   }
 
   return (
-    <div className="game">
-      <div className="game-board">
-        <Board
-          xIsNext={xIsNext}
-          squares={currentSquares}
-          onPlay={handlePlay}
-          onRestart={handleRestartClick}
-        />
-      </div>
+    <div>
+      <Board
+        xIsNext={xIsNext}
+        squares={currentSquares}
+        onPlay={handlePlay}
+        onRestart={handleRestartClick}
+      />
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol className="jump-button-lists">{move}</ol>
       </div>
     </div>
   )
